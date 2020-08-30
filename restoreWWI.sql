dc3126fb5c054d2cd78c140c3f700e9b4d6128d5
david <github.10.dmil@xoxy.net>
2020-08-30 16:28:17 -0700

    hello world

---
+++ restoreWWI.sql
@@ -0,0 +1,11 @@
+﻿RESTORE FILELISTONLY 
+FROM DISK = 'd:\projects\SSDT\WideWorldImporters-Standard.bak'
+
+RESTORE Database WideWorldImporters
+FROM DISK = 'd:\projects\SSDT\WideWorldImporters-Standard.bak'
+WITH MOVE 'WWI_Primary' TO 'd:\projects\SSDT\WideWorldImporters.mdf',
+MOVE 'WWI_Log' TO 'd:\projects\SSDT\WideWorldImporters.ldf',
+MOVE 'WWI_UserData' TO  'd:\projects\SSDT\WideWorldImporters.ndf',
+REPLACE
+
+--https://hanskindberg.wordpress.com/2016/11/08/restore-a-sql-server-backup-to-localdb/
