ea49a02e05d61acb6fe408f3861abe82c4f2827d
david <github.10.dmil@xoxy.net>
2020-08-30 17:26:26 -0700

    add odata

---
+++ solverweb/Controllers/DemoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNet.OData;
+
+namespace solverweb.Controllers
+{
+    
+    
+    public class DemoController : ControllerBase
+    {
+        [EnableQuery(PageSize = 3)]
+        public IQueryable<Demo> Get()
+        {
+            return new List<Demo>()
+            {
+                new Demo(){Key="a", Property1="the first"},
+                new Demo(){Key="b", Property1="the next"},
+                new Demo(){Key="c", Property1="more"},
+                new Demo(){Key="d", Property1="yet more"}
+            }.AsQueryable();
+        }
+
+
+    }
+
+    public class Demo
+    {
+        public string Key { get; set; }
+        public string Property1 { get; set; }
+    }
+}
--- solverweb/Startup.cs
+++ solverweb/Startup.cs
@@ -11,6 +11,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.OData.Edm;
+using Microsoft.AspNet.OData.Builder;
+using solverweb.Controllers;
 
 namespace solverweb
 {
@@ -27,6 +33,11 @@ namespace solverweb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddOData();
+            services.AddODataQueryFilter();
+
+                
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,18 +54,33 @@ namespace solverweb
 
             app.UseAuthorization();
 
+            
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapODataRoute("odata", "odata", GetEdmModel());
+                endpoints.Filter().MaxTop(100);
                 endpoints.MapGet("{*path}", (HttpContext context)=> {
                     context.Response.ContentType = "text/plain";
                     context.Response.StatusCode = 200;
                     var path = context.Request.RouteValues["path"]??"null";
                     return context.Response.WriteAsync($"hello world ({path})");
                 });
+                
             });
 
             
+        }
+        public IEdmModel GetEdmModel()
+        {
+            var odataBuilder = new ODataConventionModelBuilder();
+
+            var demo = odataBuilder.EntitySet<Demo>("Demo");
+            demo.EntityType.HasKey(d => d.Key);
+
+            return odataBuilder.GetEdmModel();
+
         }
     }
 }
